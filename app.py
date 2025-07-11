107a6b8b24a0d507785dd82e5d53bfe1ca526c1d
Muhammad Haris <102047958+MH612188-DS@users.noreply.github.com>
2025-07-11 06:07:42 +0500

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ st.markdown("Answer the following questions to assess symptom risk using WHO-bas
 
 @st.cache_resource
 def initialize_engine():
-    GOOGLE_API_KEY = AIzaSyA7cksPwuOo56k8zLTF3es20F7v66NMccg
     os.environ["GOOGLE_API_KEY"] = st.secrets["GOOGLE_API_KEY"]
     llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0.3)
     Settings.llm = llm
